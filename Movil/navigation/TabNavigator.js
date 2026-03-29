bd5f856004f0f736cf4bac7b786cfc62da74cd0b
Mayy04 <124048822@upq.edu.mx>
2026-03-29 07:34:14 -0600

    Update tabNavigator

--- Movil/navigation/TabNavigator.js
+++ Movil/navigation/TabNavigator.js
@@ -1,17 +1,17 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
 import Dashboard from "../screens/DashboardScreen";
 import Vacantes from "../screens/VacantesScreen";
 import Notificaciones from "../screens/NotificacionesScreen";
 
 const Tab = createBottomTabNavigator();
 
-export default function TabNavigator(){
-  return(
+export default function TabNavigator({ route }) {
+  const { token, nombre, id_empleado } = route.params ?? {};
+  return (
     <Tab.Navigator>
-      <Tab.Screen name="Dashboard" component={Dashboard} />
-      <Tab.Screen name="Vacantes" component={Vacantes} />
+      <Tab.Screen name="Dashboard" component={Dashboard} initialParams={{ nombre }} />
+      <Tab.Screen name="Vacantes" component={Vacantes} initialParams={{ token, id_empleado }} />
       <Tab.Screen name="Notificaciones" component={Notificaciones} />
     </Tab.Navigator>
-  )
+  );
 }
